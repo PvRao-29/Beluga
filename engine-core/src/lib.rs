3c1c8647226f72f25915bd3c1726d6006da2b480
Pranshu Rao <29.pranshurao@gmail.com>
2026-05-24 17:55:00 -0700

    Add SEE, transposition table, and time manager
    Swap-list SEE, lockless TT, and soft/hard clock limits.

--- engine-core/src/lib.rs
+++ engine-core/src/lib.rs
@@ -6,10 +6,15 @@ pub mod chess_move;
 pub mod movegen;
 pub mod perft;
 pub mod position;
+pub mod see;
+pub mod timeman;
+pub mod tt;
 pub mod types;
 pub mod zobrist;
 
 pub use bitboard::Bitboard;
 pub use chess_move::{Move, MoveFlag, MoveList};
 pub use position::{Position, START_FEN};
+pub use timeman::Limits;
+pub use tt::Tt;
 pub use types::{CastlingRights, Color, Piece, PieceType, Square};
